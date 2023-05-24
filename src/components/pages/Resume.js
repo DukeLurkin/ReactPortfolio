12b5f0cbcc80fa0c34b5eb4cbee8748893c5b486
DukeLurkin <durkin.lukas@gmail.com>
2023-05-24 15:14:06 -0400

    debugged resume download

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import "../../styles/Resume.css";
-import pdf from "../../../src/resume.pdf"
+import pdf from "../images/resume.pdf"
 
 export default function Resume() {
   return (
     <div className="Resume">
       <h1>Resume</h1>
+      <h3>
       <a href={pdf}>Download</a>
+      </h3>
       <p>
-        **Resume pdf coming soon**
+        **^^Click "Download" to download a .pdf copy of my resume Click^^**
       </p>
       <p>Proficencies
         <li>C++</li>
